d075543939b544389f7702a2fb19dc09f42a1ac6
dani <dani.zaghian@gmail.com>
2020-09-15 13:36:52 -0700

    inital commit

---
+++ db/migrate/20200915203639_create_properties.rb
@@ -0,0 +1,18 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.text :description
+      t.string :year_built
+      t.integer :square_feet
+      t.integer :bedrooms
+      t.float :bathrooms
+      t.integer :floors
+      t.boolean :availability
+      t.decimal :price, precision: 9, scale: 2
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
